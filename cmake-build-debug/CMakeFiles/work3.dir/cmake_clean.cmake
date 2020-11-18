52a6723dec6f01c6395ebc699473c62e94ce560b
kguniverse <kkwang@outlook.com>
2020-11-18 09:54:23 +0800

    update

---
+++ cmake-build-debug/CMakeFiles/work3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/work3.dir/main.cpp.o"
+  "work3"
+  "work3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/work3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/work3.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for work3.
+# This may be replaced when dependencies are built.
